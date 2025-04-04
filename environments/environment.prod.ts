4ffd8e110e9b2acaaa56c4b7321482b3c70d77b3
TroianB <136427600+TroianB@users.noreply.github.com>
2025-04-04 21:01:55 +1300

    Add files via upload

---
+++ environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: true,
+    apiUrl: 'https://tourism-dklb.onrender.com/api',
+    // apiUrl:'https://tourism-backend-1-tw9x.onrender.com/api',
+    featureXEnabled: false
+};
---
+++ environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: false,
+    apiUrl: 'https://tourism-dklb.onrender.com/api',
+    // apiUrl: 'http://localhost:4000/api',
+    featureXEnabled: true
+};
